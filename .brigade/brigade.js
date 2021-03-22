bf7b283598b8c1dae6fb3fe391e43cf2d76279ff
itowlson <ivan.towlson@microsoft.com>
2021-03-22 13:18:13 +1300

    Structured payload

--- .brigade/brigade.js
+++ .brigade/brigade.js
@@ -1,13 +1,16 @@
 const { events, Job } = require("@brigadecore/brigadier");
 
 events.on("slack", "slash_command", async event => {
+
+    const command = JSON.parse(event.payload);
+
     let fooJob = new Job("foo", "debian:latest", event);
     fooJob.primaryContainer.command = ["echo"];
-    fooJob.primaryContainer.arguments = [event.payload];
+    fooJob.primaryContainer.arguments = [command.native];
     await fooJob.run();
 
     let barJob = new Job("bar", "debian:latest", event);
     barJob.primaryContainer.command = ["echo"];
-    barJob.primaryContainer.arguments = ["barrrrrrrrrrrrrr"];
+    barJob.primaryContainer.arguments = [`${command.command} ${command.text} (from ${command.channel})`];
     await barJob.run();
 });
