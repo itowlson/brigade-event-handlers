7055929da59080115d5153b1687a46c6f448ef1e
itowlson <ivan.towlson@microsoft.com>
2021-03-26 13:50:41 +1300

    Use the Slack types

--- .brigade/brigade.js
+++ .brigade/brigade.js
@@ -4,43 +4,32 @@ const { events, logger, Job } = require("@brigadecore/brigadier");
 events.on("slack", "slash_command", async event => {
 
     const command = JSON.parse(event.payload);
-    // logger.info(`payload=${event.payload}`);
-    // logger.info(`native=${command.native}`);
 
     // TODO: package.json doesn't work at the moment
-    // const slack = new WebClient(command.token);
-    // const conversationId = command.channelId;
+    // const slack = new WebClient(command.responseToken);
+    // const conversationId = command.body.channel_id;
     // logger.info('notifying Slack');
-    // await slack.chat.postMessage({ channel: conversationId, text: `Brigade has received your command regarding ${command.text} and will ${command.command.substr(1)} it immediately` });
+    // await slack.chat.postMessage({ channel: conversationId, text: `Brigade has received your command regarding ${command.body.text} and will ${command.body.command.substr(1)} it immediately` });
     // logger.info('notified Slack');
 
     let fooJob = new Job("foo", "debian:latest", event);
     fooJob.primaryContainer.command = ["echo"];
-    fooJob.primaryContainer.arguments = [JSON.stringify(command.native)];
+    fooJob.primaryContainer.arguments = [JSON.stringify(command)];
     await fooJob.run();
 
     let barJob = new Job("bar", "debian:latest", event);
     barJob.primaryContainer.command = ["echo"];
-    barJob.primaryContainer.arguments = [`${command.command} ${command.text} (from ${command.channel})`];
+    barJob.primaryContainer.arguments = [`${command.body.command} ${command.body.text} (from ${command.body.channel_name})`];
     await barJob.run();
 });
 
 events.on("slack", "shortcut", async event => {
 
     const shortcut = JSON.parse(event.payload);
-    // logger.info(`payload=${event.payload}`);
-    // logger.info(`native=${command.native}`);
-
-    // TODO: package.json doesn't work at the moment
-    // const slack = new WebClient(command.token);
-    // const conversationId = command.channelId;
-    // logger.info('notifying Slack');
-    // await slack.chat.postMessage({ channel: conversationId, text: `Brigade has received your command regarding ${command.text} and will ${command.command.substr(1)} it immediately` });
-    // logger.info('notified Slack');
 
     let fooJob = new Job("foo", "debian:latest", event);
     fooJob.primaryContainer.command = ["echo"];
-    fooJob.primaryContainer.arguments = [JSON.stringify(shortcut.native)];
+    fooJob.primaryContainer.arguments = [JSON.stringify(shortcut)];
     await fooJob.run();
 
     let barJob = new Job("bar", "debian:latest", event);
@@ -52,23 +41,14 @@ events.on("slack", "shortcut", async event => {
 events.on("slack", "message_action", async event => {
 
     const shortcut = JSON.parse(event.payload);
-    // logger.info(`payload=${event.payload}`);
-    // logger.info(`native=${command.native}`);
-
-    // TODO: package.json doesn't work at the moment
-    // const slack = new WebClient(command.token);
-    // const conversationId = command.channelId;
-    // logger.info('notifying Slack');
-    // await slack.chat.postMessage({ channel: conversationId, text: `Brigade has received your command regarding ${command.text} and will ${command.command.substr(1)} it immediately` });
-    // logger.info('notified Slack');
 
     let fooJob = new Job("foo", "debian:latest", event);
     fooJob.primaryContainer.command = ["echo"];
-    fooJob.primaryContainer.arguments = [JSON.stringify(shortcut.native)];
+    fooJob.primaryContainer.arguments = [JSON.stringify(shortcut)];
     await fooJob.run();
 
     let barJob = new Job("bar", "debian:latest", event);
     barJob.primaryContainer.command = ["echo"];
-    barJob.primaryContainer.arguments = [`${shortcut.text} (from ${shortcut.channel})`];
+    barJob.primaryContainer.arguments = [`${shortcut.body.message.text} (from ${shortcut.body.channel.name})`];
     await barJob.run();
 });
